01025ad59b874f276e6615d692327613ffbb7fd0
khenryjoe <joseph.ketterer@gmail.com>
2020-08-10 14:21:27 +0100

    d

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,11 @@ import { getData } from "../data/data";
 export const fetchPosts = (data, method) => async (dispatch) => {
     const response = await getData()
     const x =response.data[data].filter(function(item){
-        return item.name == method;})      
+        return item.name === method;})      
     dispatch({type: "FETCH_POSTS", payload: x[0]});
  };
+
+ export const getMethod = (value, dispatch) => {
+
+    dispatch({type: "GET_METHOD", payload: value})
+ }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 export default (state =[], action) => {
     switch(action.type){
-       case 'FETCH_POSTS':
+        case 'FETCH_POSTS':
            return action.payload;
+        case 'GET_METHOD':
+            return action.payload;
         default: 
         return state;
      
