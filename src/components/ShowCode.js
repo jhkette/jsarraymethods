2c78966920b789fca3b2bee7b86ce349ed575c54
khenryjoe <joseph.ketterer@gmail.com>
2020-10-15 12:52:43 +0100

    typewriter kind of working

--- src/components/ShowCode.js
+++ src/components/ShowCode.js
@@ -1,22 +1,27 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import Typist from "react-typist";
+import Typer from './Typewriter'
+
 import { fetchPosts } from "../actions";
 
 class ShowCode extends Component {
+ 
   // runs every time state changes
   componentDidUpdate(prevProps) {
     if (prevProps.methods !== this.props.methods) {
       this.props.fetchPosts(this.props.operation, this.props.methods);
     }
   }
+
   createMarkup() {
     return { __html: this.props.posts.example };
   }
+  
 
   renderText() {
-   
-    return this.props.posts && this.props.operation ? (
+    // console.log(this.props.posts.output)
+
+    return this.props.posts.output && this.props.operation ? (
       <div>
         <div
           className="show-code"
@@ -24,11 +29,7 @@ class ShowCode extends Component {
         />
 
         <div className="show-code">
-          {this.props.posts.desc ? (
-            <Typist> <Typist.Delay ms={450}/>{this.props.posts.output}</Typist>
-          ) : (
-            ""
-          )}
+          <Typer heading={'Things I want to type:'} dataText={[...this.props.posts.output]} />
         </div>
       </div>
     ) : (
