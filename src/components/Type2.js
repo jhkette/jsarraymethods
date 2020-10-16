3b6a07d49e11ab720fbbf1d42f349dae23b4c9ee
khenryjoe <joseph.ketterer@gmail.com>
2020-10-16 16:49:51 +0100

    still needs amending

--- src/components/Type2.js
+++ src/components/Type2.js
@@ -2,61 +2,62 @@ import React, { Component } from "react";
 
 class TypeWriter extends Component {
   constructor(props) {
-    super(props)
-   
+    super(props);
+
     this.state = {
       txt: "",
       wordIndex: 0,
       wait: parseInt(1000, 10),
       isDeleting: false,
     };
-    this.type()
+    this.type();
   }
- 
+  componentDidUpdate() {
+    // if (prevProps.methods !== this.props.methods) {
+    // }
+  }
+
   type() {
     // Current index of word
-    if(this.props.dataTxt){
-    // Get full text of current word
-    const fullTxt = this.props.dataTxt
-    if (this.state.isDeleting) {
-      // Remove char
-      this.setState(prevState => {
-       return {txt: fullTxt.substring(0, prevState.txt.length - 1)}
-      });
-    } else {
-      // Add char
-      this.setState(prevState => {
-       return {txt: fullTxt.substring(0, prevState.txt.length + 1)}
-      });
+    if (this.props.dataTxt) {
+      // Get full text of current word
+      const fullTxt = this.props.dataTxt;
+      if (this.state.isDeleting) {
+        // Remove char
+        this.setState((prevState) => {
+          return { txt: fullTxt.substring(0, prevState.txt.length - 1) };
+        });
+      } else {
+        // Add char
+        this.setState((prevState) => {
+          return { txt: fullTxt.substring(0, prevState.txt.length + 1) };
+        });
+      }
+      let typeSpeed = 250;
+      if (this.state.isDeleting) {
+        typeSpeed /= 2;
+      }
+      // If word is complete
+      if (!this.state.isDeleting && this.state.txt === fullTxt) {
+        // Make pause at end
+        typeSpeed = this.state.wait;
+        // Set delete to true
+        // this.setState({ isDeleting: true });
+      } else if (this.state.isDeleting && this.state.txt === "") {
+        this.setState({ isDeleting: false });
+        // Move to next word
+        this.setState((prevState) => {
+          return { wordIndex: prevState.wordIndex + 1 };
+        });
+        // Pause before start typing
+        typeSpeed = 500;
+      }
+      setTimeout(() => this.type(), typeSpeed);
     }
-    let typeSpeed = 100;
-    if (this.state.isDeleting) {
-      typeSpeed /= 2;
-    }
-    // If word is complete
-    if (!this.state.isDeleting && this.state.txt === fullTxt) {
-      // Make pause at end
-      typeSpeed = this.state.wait;
-      // Set delete to true
-      this.setState({isDeleting: true})
-    } else if (this.state.isDeleting && this.state.txt === "") {
-      this.setState({isDeleting: false})
-      // Move to next word
-      this.setState(prevState => {
-        return {wordIndex: prevState.wordIndex +1 }
-      })
-      // Pause before start typing
-      typeSpeed = 500;
-    }
-    setTimeout(() => this.type(), typeSpeed);
-   }
   }
-  render(){
-    
-   console.log(this.state.txt)
-   return (
-    <span>{this.state.txt}</span>
-   )
+  render() {
+    console.log(this.state.txt);
+    return <span>{this.state.txt}</span>;
   }
 }
 
